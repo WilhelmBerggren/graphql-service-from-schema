39489724a922e70dfcae076a19ab676cf34dab6b
Wilhelm Berggren <wilhelm.berggren@hiberworld.com>
2022-06-12 01:26:01 +0200

    Use same directive for all SQL

--- parse.ts
+++ parse.ts
@@ -15,22 +15,19 @@ const schema = fs.readFileSync("schema.graphql").toString();
 
 const parsed = parse(schema);
 
-const sqlCreateTableCommands: { [key: string]: string } = {};
-const sqlInsertCommands: { [key: string]: string } = {};
-const sqlFindAllCommands: { [key: string]: string } = {};
-const sqlFindCommands: { [key: string]: string } = {};
+const sqlCommands: { [key: string]: string } = {};
 
 const visitor: ASTVisitor = {
   ObjectTypeDefinition: {
     enter(node) {
       const sqlCreateDirective = node.directives?.find(
-        (d) => d.name.value === "SQLCreateTable"
+        (d) => d.name.value === "SQL"
       );
       const createCommand = sqlCreateDirective?.arguments?.find(
         (a) => a.name.value === "command"
       );
       if (createCommand) {
-        sqlCreateTableCommands[node.name.value] = (
+        sqlCommands["Object-" + node.name.value] = (
           createCommand.value as StringValueNode
         ).value;
       }
@@ -39,38 +36,14 @@ const visitor: ASTVisitor = {
   FieldDefinition: {
     enter(node) {
       const sqlFindAllDirective = node.directives?.find(
-        (d) => d.name.value === "SQLFindAll"
+        (d) => d.name.value === "SQL"
       );
       const findAllCommand = sqlFindAllDirective?.arguments?.find(
         (a) => a.name.value === "command"
       );
       if (findAllCommand) {
-        sqlFindAllCommands[node.name.value] = (
+        sqlCommands["Field-" + node.name.value] = (
           findAllCommand.value as StringValueNode
-        ).value;
-      }
-
-      const sqlFindDirective = node.directives?.find(
-        (d) => d.name.value === "SQLFind"
-      );
-      const findCommand = sqlFindDirective?.arguments?.find(
-        (a) => a.name.value === "command"
-      );
-      if (findCommand) {
-        sqlFindCommands[node.name.value] = (
-          findCommand.value as StringValueNode
-        ).value;
-      }
-
-      const sqlInsertDirective = node.directives?.find(
-        (d) => d.name.value === "SQLInsert"
-      );
-      const insertCommand = sqlInsertDirective?.arguments?.find(
-        (a) => a.name.value === "command"
-      );
-      if (insertCommand) {
-        sqlInsertCommands[node.name.value] = (
-          insertCommand.value as StringValueNode
         ).value;
       }
     },
@@ -80,7 +53,9 @@ const visitor: ASTVisitor = {
 visit(parsed, visitor);
 
 const db = new Sqlite("sqlite.db", { verbose: console.log });
-for (let command of Object.values(sqlCreateTableCommands)) {
+for (let command of Object.values(sqlCommands).filter((command) =>
+  command.startsWith("create table")
+)) {
   console.log(db.prepare(command).run());
 }
 
@@ -89,14 +64,14 @@ const server = new ApolloServer({
   resolvers: {
     Query: {
       user: (_, { id }) => {
-        return db.prepare(sqlFindCommands["user"]).get(id);
+        return db.prepare(sqlCommands["Field-user"]).get(id);
       },
     },
     Mutation: {
       createUser: (_, { username }) => {
         const id = Math.random().toString();
-        console.log({ sqlInsertCommands });
-        db.prepare(sqlInsertCommands["createUser"]).run(id, username);
+        console.log({ sqlCommands });
+        db.prepare(sqlCommands["Field-createUser"]).run(id, username);
         return {
           id,
           username,
